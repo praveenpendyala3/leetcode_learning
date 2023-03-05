cd5f71c8f9136a6a6c743eab0145e5b1899aabc2
praveen <pendyala.praveen@gmail.com>
2023-03-05 14:27:44 -0800

    added dewsc

--- DAA_dp_assignment/q5_lc55_jump_game_dp_botup.py
+++ DAA_dp_assignment/q5_lc55_jump_game_dp_botup.py
@@ -1,8 +1,8 @@
 
 
-#jmpto(i) = Is it possible to jump to position i in any way?
-
-# we look at all indexes that we can possibly jump from and recrsively check if that index can be jumped to from a previous index 
+# Problem --> dp[maxReach] == can "maxReach"th index be reached? 
+# Problem to subproblems --> if 'i' can be reached (dp[i] ==True ) and if we can jump from 'i' to 'maxReach', then 'maxReach' can be reached (dp[maxReach] =True). 
+# Subproblems --> Thus, dp[i] is subproblem, where i<maxReach.
 
 def canJump(nums):
     dp = [False for _ in range(len(nums))]
@@ -11,6 +11,8 @@ def canJump(nums):
     i=0
     for maxReach in range(1,len(nums)):
         while i < maxReach:
+
+
             if dp[i] == True and nums[i] + i >= maxReach:
                 dp[maxReach] = True
                 break
@@ -19,5 +21,5 @@ def canJump(nums):
     return dp[-1]
 
 
-
+##### Testcases ######
 print( canJump([2,3,1,1,4]))
